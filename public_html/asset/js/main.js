bd46f83daa968871cc54def5085a39aa51fac0f8
Leonardo Ralph <theleoralph@gmail.com>
2022-01-04 11:53:27 -0300

    adicionando novos arquivos em public_html

--- public_html/asset/js/main.js
+++ public_html/asset/js/main.js
@@ -10,6 +10,39 @@ $(document).ready(function(){
         }).done(function(result){
             $('#myModal').html(result)
             $('#myModal').modal('show')
-        })
-    })
-})
+        });
+    });
+
+    $('.excluir-item').click(function(){
+        $.ajax({
+            url: $('#_url').val() + '/excluir',
+            data: {
+                _csrf: $('#_csrf').val(),
+                case: $(this).attr('case'),
+                target: $(this).attr('id')
+            },
+            method: 'GET'
+        }).done(function(result){
+            $('#myModal').html(result);
+            $('#myModal').modal('show');
+        });
+    });
+
+    $("#pesquisar").on("keyup", function() {
+        var busca = $(this).val().toLowerCase();
+    
+        $("table tr").each(function(index) {
+            if (index !== 0) {
+                var tds = $(this).find("td");
+                var textoTd = tds.text().toLowerCase();
+                
+                if (textoTd.includes(busca)){
+                    $(this).show();
+                }
+                else {
+                    $(this).hide();
+                }
+            }
+        });
+    });
+});
